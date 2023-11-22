b2f39b3a2e26fb3e7308802c866c7d80b50ec26a
Akash kuamr <akashkumarmaurya65@gmail.com>
2023-11-22 23:51:34 +0530

    akash

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in! Akash Kumar Maurya') }}
                 </div>
             </div>
         </div>
